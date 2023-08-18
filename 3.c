82df7137e24610bedff471e5aef54851efa379f3
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:05:32 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int x=12, y=90;
+    //printf("\n x =%d \n y=%d",x,y);
+    //printf("\n x =%5d \n y=%5d",x,y);
+    printf("\n x=%-5d \n y=%-5d",x,y);
+    getch();
+
+}

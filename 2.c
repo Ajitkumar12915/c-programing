c799ec1a2861df22b33c9cfd7892ed03bdc9c049
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:04:54 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ //int x=10, y=30; //z=100, a=500, b=100;
+    int l;
+    //l=printf("hello");
+    //printf("%d",l);
+    //printf("%d\n%d\n%d\n%d\n%d\n",x,y,z,a,b);
+   // printf("%d %d %d %d %d \n",x,y,z,a,b);
+    //printf("x=%d \n y=%d", x,y);
+    //printf("v1=%d\nv2=%d", x,y);
+    //printf("\n bye");
+    //printf(" \n hello");
+   // printf("\n bye");
+   // printf("\n hello");
+   // printf("\n bye");
+    //printf("\n hello");
+    //printf("\n bye");
+    getch();
+}

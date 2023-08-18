79f5536a18d83daeb1f6eeb361a0482fd1c0a854
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:12:03 +0530

    Add files via upload

---
+++ subraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a, b, sub;
+    printf("enter number a=");
+    scanf("%d", &a);
+    printf("enter number b=");
+    scanf("%d", &b);
+    sub = a - b;
+    printf("sub is:%d", sub);
+    return 0;
+}

2ebd9b5da6a92e7660efe98ae0c4462b2c3b733a
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:10:29 +0530

    Add files via upload

---
+++ divid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a, b, divide;
+    printf("Enter the number a=");
+    scanf("%d", &a);
+    printf("Enter the number b=");
+    scanf("%d", &b);
+    divide = a / b;
+    printf("divide is:%d", divide);
+    return 0;
+}

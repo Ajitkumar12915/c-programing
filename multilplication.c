97010f370ea892a2ee8724af25e551e82c6b2f5e
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:11:21 +0530

    Add files via upload

---
+++ multilplication.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a, b;
+    printf("enter number a=");
+    scanf("%d", &a);
+    printf("enter number b=");
+    scanf("%d", &b);
+    int mul = a * b;
+    printf("mul is:%d", mul);
+    return 0;
+}

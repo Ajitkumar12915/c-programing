90e3a3d744ecf6bb269ad8c187d8cd302c2c527f
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:04:33 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    //  printf("hello to everyone in the world");
+    //  printf("\n");
+    //  printf("hello to ajit kumar");
+    //  printf("\n");
+    //  printf("hello to everyone in the anshu");
+    //  printf("\n");
+    //  printf("hello to everyone in the ram");
+    //  printf("\n");
+    //  printf("hello to everyone in the shayam");
+    //  printf("\n");
+    //  printf("hello to everyone in the mohan");
+    //  printf("\n");
+    //  printf("hello to everyone in the radha");
+    //  printf("\n");
+    //  printf("hello to everyone in the sohan");
+    //  printf("\n");
+    //  printf("hello to everyone in the komal");
+    //  printf("\n");
+    //  printf("hello to everyone in the kamal");
+    //  printf("hello to everyone in the world");
+    //  printf("\n");
+    //  printf("hello to ajit kumar");
+    //  printf("\n");
+    //  printf("hello to everyone in the anshu");
+    //  printf("\n");
+    //  printf("hello to everyone in the ram");
+    //  printf("\n");
+    //  printf("hello to everyone in the shayam");
+    //  printf("\n");
+    //  printf("hello to everyone in the mohan");
+    //  printf("\n");
+    printf("hello to everyone  \n radha");
+    getch();
+}

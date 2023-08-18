e0349793b54f212731638acaf9caf824788cc29d
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:09:47 +0530

    Add files via upload

---
+++ area_of_squre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int side, area;
+    printf("Enter the side=");
+    scanf("%d", &side);
+    area = side * side;
+    printf("area of squre is:%d", area);
+    return 0;
+}

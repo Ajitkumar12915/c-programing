85473c1c1e6e2ffbeec15a735e2e76523698598e
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:06:47 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch='G';
+    //printf("\n %c",ch);
+    //printf("\n character%c",ch);
+    printf("\n ASCII=%d",ch);
+    getch();
+}

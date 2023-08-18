b50c71edd031feac841a6f91fab75d63ac7bc1a0
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2023-08-18 11:10:11 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,15 @@
+// finding the cube of any number.if the number is give.a=9.(// slash is using for single line comments).
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int num, cube;
+    printf("enter the num=");
+    scanf("%d", &num);
+    cube = num * num * num;
+    printf("cube of any number is:%d", cube);
+    return 0;
+}
+/* This slah and astric sign is used for multiline comments
+open with slash astric and close with astric lash.*/
